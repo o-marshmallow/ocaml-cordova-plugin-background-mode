d86df66ae7406efbffab611ec9ae76c596392420
Omar <elachich@informatique.univ-paris-diderot.fr>
2016-07-22 10:53:25 +0200

    Adding binding and opam files

---
+++ Makefile
@@ -0,0 +1,16 @@
+NAME=cordova_background
+PACKAGE=-package gen_js_api,js_of_ocaml
+all:
+	ocamlfind gen_js_api/gen_js_api $(NAME).mli
+	ocamlfind ocamlc $(PACKAGE) $(NAME).mli
+	ocamlfind ocamlc -c -package gen_js_api.ppx $(PACKAGE) $(NAME).ml
+	ocamlfind ocamlc -a -package gen_js_api.ppx -o $(NAME).cma $(NAME).cmo
+
+install: all
+	ocamlfind install ocaml-cordova-plugin-background-mode META *.cma *.cmi
+
+mrproper:
+	rm -rf maps *.cmi *.cmo *.cma
+
+clean:
+	rm -rf $(NAME) $(NAME).ml *.cmi *.cmo *.cma
---
+++ cordova_background.mli
@@ -0,0 +1,64 @@
+val enable : unit -> unit
+  [@@js.global "cordova.plugins.backgroundMode.enable"]
+
+val disable : unit -> unit
+  [@@js.global "cordova.plugins.backgroundMode.disable"]
+
+val is_enabled : unit -> bool
+  [@@js.global "cordova.plugins.backgroundMode.isEnabled"]
+
+val is_active : unit -> bool
+  [@@js.global "cordova.plugins.backgroundMode.isActive"]
+
+val onactivate : unit -> unit
+  [@@js.global "cordova.plugins.backgroundMode.onactivate"]
+
+val set_onactivate : (unit -> unit) -> unit
+  [@@js.set "cordova.plugins.backgroundMode.onactivate"]
+
+val ondeactivate : unit -> unit
+  [@@js.global "cordova.plugins.backgroundMode.ondeactivate"]
+
+val set_ondeactivate : (unit -> unit) -> unit
+  [@@js.set "cordova.plugins.backgroundMode.ondeactivate"]
+
+val onfailure : unit -> unit
+  [@@js.global "cordova.plugins.backgroundMode.onfailure"]
+
+val set_onfailure : (unit -> unit) -> unit
+  [@@js.set "cordova.plugins.backgroundMode.onfailure"]
+
+type config
+
+val create_config :
+  ?title:string ->
+  ?ticker:string ->
+  ?text:string ->
+  ?resume:bool ->
+  ?silent:bool ->
+  unit ->
+  config
+  [@@js.builder]
+val title : config -> string
+val ticker : config -> string
+val text : config -> string
+val resume : config -> bool
+val silent : config -> bool
+val set_title : config -> string -> unit
+val set_ticker : config -> string -> unit
+val set_text : config -> string -> unit
+val set_resume : config -> bool -> unit
+val set_silent : config -> bool -> unit
+
+
+val set_defaults : config -> unit
+  [@@js.global "cordova.plugins.backgroundMode.setDefaults"]
+
+val get_defaults : unit -> config
+  [@@js.global "cordova.plugins.backgroundMode.getDefaults"]
+
+val configure : config -> unit
+  [@@js.global "cordova.plugins.backgroundMode.configure"]
+
+val log : string -> unit
+  [@@js.global "console.log"]
